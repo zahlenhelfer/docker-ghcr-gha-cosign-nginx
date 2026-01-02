30c113ff53508f337fe49da0c0ebee238dcea696
Marcus Ross <marcus.ross@zahlenhelfer.de>
2026-01-02 13:01:11 +0100

    chore: changing cluster policy to regex

--- kyverno-example/00-setup.sh
+++ kyverno-example/00-setup.sh
@@ -15,6 +15,19 @@ kubectl apply -f verify-zh-ghcr-images.yaml
 
 kubectl get clusterpolicy
 
+kubectl run test \
+-n signed \
+--image ghcr.io/zahlenhelfer/docker-ghcr-gha-cosign-nginx:latest
+
+kubectl run test \
+-n signed \
+--image ghcr.io/zahlenhelfer/docker-ghcr-gha-cosign-nginx@sha256:e8f756722068ebdfbfa45d875a1d7a90110d687825bf8feba5e5c183d2f7b320
+
+
+cosign verify \
+  --certificate-oidc-issuer https://token.actions.githubusercontent.com \
+  ghcr.io/zahlenhelfer/docker-ghcr-gha-cosign-nginx@sha256:e8f756722068ebdfbfa45d875a1d7a90110d687825bf8feba5e5c183d2f7b320
+
 
 kubectl run test \
 -n signed \
